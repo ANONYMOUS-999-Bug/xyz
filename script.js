874b17d650726388e8f7829a45f9fdde5d799c26
anonymous <dhurbal142@gmail.com>
2024-12-05 21:26:31 +0530

    finished

---
+++ script.js
@@ -0,0 +1,25 @@
+// let nameStr = document.querySelector(".name");
+// nameStr.innerText = "your name is sukdhan barman";
+// let type = document.querySelector(".input")
+// let value = type.getAttribute("type");
+// let result = type.innerHTML;
+// console.log(result);
+// let result =  document.querySelectorAll(".through");
+// let secondelement = result[2];
+// let value = secondelement.innerHTML;
+// console.log(value);
+// let setValue = document.querySelector(".through");
+// let value = setValue.setAttribute("src","https://newexample.com')");
+// let anchoretag = document.querySelectorAll("a");
+// console.log(anchoretag);
+// anchoretag[0].style.color = "teal";
+// for(let i = 0; i <= anchoretag.length; i++){
+//     anchoretag[i].style.cssText=
+    
+// }
+let anchoretag = document.querySelectorAll("a")
+anchoretag.forEach((a)=>{
+    a.setAttribute("href","https://youtube.com/shorts/61uem2JCvSM?feature=shared")
+})
+let button = document.querySelector(".button");
+
